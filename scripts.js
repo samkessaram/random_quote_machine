b16af1ef11650213c781657572e2661c30f20434
samkessaram <samkessaram@gmail.com>
2016-12-30 18:52:33 +0000

    canvas height set to window inner height

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ $(function(){
   getQuote();
     
     function getQuote(){
+      $('#canvas').css('height',window.innerHeight);
       var timestamp = Date.now();
       var url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&' + timestamp;
       $.getJSON(url,function(json){
